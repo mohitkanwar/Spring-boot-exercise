1dc5268ab681db5e863e86619cbb4d05edfe8fe3
Mohit Kanwar <m.m.kanwar@gmail.com>
2019-05-03 10:20:13 +0530

    initial setup

---
+++ src/main/java/com/mk/spring/tutorial/exercisespringboot/ExerciseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mk.spring.tutorial.exercisespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseSpringBootApplication.class, args);
+	}
+
+}
